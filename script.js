3cd1e9426ff6f14eb724cb7dc331fcd4670e1ef2
nexelio321 <nexeliomarketing26@gmail.com>
2025-02-11 21:03:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('explore-btn').addEventListener('click', function() {
+    alert('Exploring services...');
+    // You can add more functionality here, like redirecting to another page
+});
